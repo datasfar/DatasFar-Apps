368401d921d1f3cb00c1b986966fda9afa5fa6a6
Datasfar <datasfar@gmail.com>
2024-05-02 18:19:01 +0200

    yt downloader working

--- apps/04_youtube_utilities/downloader.py
+++ apps/04_youtube_utilities/downloader.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from pytube import YouTube
+
+# Ya no es necesario el ssl (?)
 import ssl
 ssl._create_default_https_context = ssl._create_stdlib_context
 
@@ -13,11 +15,26 @@ class Downloader():
 
         download_button = st.button("Descargar")
 
-        if download_button:
-            
+        if url != "":
             youtube = YouTube(url)
 
-            video = youtube.streams.get_by_resolution(resolution)
-            video.download("./Descargas")
-            st.success("Video descargado con exito")
+            # extras
+            col1, col2 = st.columns(2)
 
+            with col1:
+                st.write(youtube.title, "Subido por:", youtube.author)
+                st.write(str(youtube.views), "views")
+                st.write("Subido el:", str(youtube.publish_date))
+                st.write("Duración: ", youtube.length, "sec")
+
+            with col2:
+                st.image(youtube.thumbnail_url)
+
+        if download_button:
+
+            video = youtube.streams.get_by_resolution(resolution)
+
+            video.download("./Descargas")
+            st.success("Video descargado correctamente")
+
+        ## Agregar opción para descargar solo el audio
--- apps/04_youtube_utilities/main.py
+++ apps/04_youtube_utilities/main.py
@@ -1,15 +1,16 @@
 import streamlit as st
 
 from downloader import Downloader
+from transcript import Transcripter
 
 
 def main():
-    st.header("Aplicación para corregir & traducir textos.")
-    st.write("La aplicación permite corregir la ortografía y traducir textos.")
+    st.header("Aplicación para descargar videos de Youtube")
+    st.write("Descarga y analiza videos de youtube")
     st.write("Desarrollada con <3 por dataSfar")
 
 # Configuración de la barra lateral
-pagina_actual = st.sidebar.radio("Utilidades", ["Inicio", "Descargador", "Traductor"])
+pagina_actual = st.sidebar.radio("Utilidades", ["Inicio", "Descargador", "Transcript"])
 
 # Mostrar la página seleccionada
 if pagina_actual == "Inicio":
@@ -17,4 +18,7 @@ if pagina_actual == "Inicio":
 elif pagina_actual == "Descargador":
     video_downloader = Downloader()
     video_downloader.video_downloader_interface()
+elif pagina_actual == "Transcript":
+    video_transcript = Transcripter()
+    video_transcript.video_transcript_interface()
 
